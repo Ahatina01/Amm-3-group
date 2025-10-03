dd4130d2ef8baafec7e37850b8deda1407830388
Ahatina01 <imost4965@gmail.com>
2025-10-03 23:27:25 +0300

    Add files via upload

---
+++ 2 занятие ооп/ClassComplex (2).cpp
@@ -0,0 +1,84 @@
+#include "ClassComplex.h"
+using namespace std;
+
+Complex::Complex()
+{
+	Re = new double(0);
+	Im = new double(0);
+	cout << "Default Constructor\n";
+}
+
+Complex::Complex(double RE, double IM)
+{
+	Re = new double(RE);
+	Im = new double(IM);
+	cout << "Constructor with parameters\n";
+}
+
+Complex::Complex(const Complex& second)
+{
+	Re = new double(*second.Re);
+	Im = new double(*second.Im);
+	cout << "Copying Constructor\n";
+}
+
+double* Complex::getRe() const
+{
+	return Re;
+}
+
+double* Complex::getIm() const
+{
+	return Im;
+}
+
+Complex::~Complex()
+{
+	delete Re;
+	delete Im;
+	cout << "Destructor\n";
+}
+
+string Complex::print()
+{
+	string str = to_string(*Re) + " + " + to_string(*Im) + "i\n";
+	return str;
+}
+
+Complex Complex::operator+(const Complex& second)
+{
+	Complex res;
+	*res.Re = *Re + *second.Re;
+	*res.Im = *Im + *second.Im;
+	cout << "Operator +\n";
+	return res;
+}
+
+Complex& Complex::operator+=(const Complex& second)
+{
+	*Re += *second.Re;
+	*Im += *second.Im;
+	cout << "Operator +=\n";
+	return *this;
+}
+
+Complex& Complex::operator=(const Complex& second)
+{
+	*Re = *second.Re;
+	*Im = *second.Im;
+	cout << "Operator =\n";
+	return *this;
+}
+
+ostream& Complex::operator<<(ostream& out)
+{
+	out << "(" << *Re << " , " << *Im << ")\n";
+	return out;
+}
+
+ostream& operator<<(ostream& out, const Complex& obj)
+{
+	//out << "(" << *obj.getRe() << " , " << *obj.getIm() << ")\n";
+	out << "(" << *obj.Re << " , " << *obj.Im << ")\n";
+	return out;
+}
